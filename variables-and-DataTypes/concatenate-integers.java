d8aae695d07f0dfdc7902f8fc6354025576f277d
RADHESHYAM KUSHWAHA <104156567+devradheee@users.noreply.github.com>
2024-01-06 21:35:16 +0000

    concatenate intenger

---
+++ variables-and-DataTypes/concatenate-integers.java
@@ -0,0 +1,7 @@
+
+public  static  void concatenate(int a, int b){
+    //Complete the code below to concatenate a and b
+    String ans =String.valueOf(a) + String.valueOf(b);
+    //Complete the code above to concatenate a and b
+    System.out.println(ans);
+}
