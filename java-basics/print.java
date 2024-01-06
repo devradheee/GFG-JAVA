0129b325ae229e2b5ad87ce7f074345c4d9ab367
RADHESHYAM KUSHWAHA <104156567+devradheee@users.noreply.github.com>
2024-01-06 20:37:27 +0000

    print

---
+++ java-basics/print.java
@@ -0,0 +1,24 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+
+// } Driver Code Ends
+//User function Template for Java
+
+class Geeks {
+
+    // comment the line that outputs "Hello World"
+    static void print() {
+
+        // Prints Hello World
+        // System.out.println("Hello World");
+        // Prints GeeksForGeeks
+        System.out.println("GeeksForGeeks");
+
+    }
+
+}
+
+// { Driver Code Starts.
